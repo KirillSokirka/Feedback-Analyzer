e26a7722497a23c36d8c6c47c37c0e0301357687
KirillSokirka <sokirka2003@gmail.com>
2024-01-11 23:13:02 +0200

    Set database contexts

--- api/Api/Program.cs
+++ api/Api/Program.cs
@@ -1,7 +1,17 @@
 using FeedbackAnalyzer.Api.OptionsSetup;
+using Identity.DbContext;
+using Identity.Seed;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.EntityFrameworkCore;
+using Persistence;
 
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddDbContext<IdentityDatabaseContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("IdentityDbConnection")));
+
+builder.Services.AddDbContext<ApplicationDatabaseContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("ApplicationDbConnection")));
 
 builder.Services
     .AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
@@ -21,6 +31,11 @@ if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
     app.UseSwaggerUI();
+}
+
+using (var scope = app.Services.CreateScope())
+{
+    await IdentityDatabaseInitializer.Initialize(scope.ServiceProvider);
 }
 
 app.UseHttpsRedirection();
---
+++ api/Persistence/ApplicationDatabaseContext.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Persistence;
+
+public class ApplicationDatabaseContext: DbContext
+{
+    public ApplicationDatabaseContext(DbContextOptions<ApplicationDatabaseContext> options) : base(options)
+    {
+    }
+}
